819011177375ec1c56b94b42266d52030ccd3d7e
paul <paul.suffel>
2023-03-15 08:58:30 +0400

    Added model to demo model classe and corresponding admin view

--- demo/models.py
+++ demo/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 # Create your models here.
 
 class Demo(models.Model):
     name= models.CharField(max_length= 200)
     start_date= models.DateField()
     responsible= models.ForeignKey(User, on_delete= models.CASCADE)
-    week_number = models.CharField
+    week_number = models.CharField(max_length= 2, blank= True)
+    end_date= models.DateField()
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        print(self.start_date.isocalendar()[1])
+        super().save(*args, **kwargs)
 
