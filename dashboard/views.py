2aa6c7f1040fe91f69508ada011df01d2c0eb0c9
paul <paul.suffel>
2023-04-28 23:50:13 +0400

    - changed gantt chart to line plot with daily revenue - added pie chart with revenue by shopping mall - added static folder with style.css but not yet working

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,37 +1,49 @@
 from django.shortcuts import render
-from demo.models import Demo
+from demo.models import Demo, Invoice
 import pandas as pd
 from plotly.offline import plot
 import plotly.express as px
+import plotly.graph_objs as go
 from demo.load_invoice import DataLoader
 # Create your views here.
 
 def index(request):
 
-    try:
-        dl= DataLoader()
-        dl.load_data()
-    except Exception as e:
-        print(e)
+    #try:
+    #    dl= DataLoader()
+    #    dl.load_data()
+    #except Exception as e:
+    #    print(e)
 
-    qs= Demo.objects.all()
-    demo_data= [
+    invoice_objects= Invoice.objects.all()
+    invoice_data= [
         {
-            'demo': x.name,
-            'start': x.start_date,
-            'end': x.end_date,
-            'responsible': x.responsible.username
-        } for x in qs
+            'date': x.invoice_date,
+            'price': x.price,
+            'shopping_mall': x.shopping_mall,
+        } for x in invoice_objects
     ]
 
-    df = pd.DataFrame(demo_data)
+    df = pd.DataFrame(invoice_data)
 
-    fig = px.timeline(
-        df, x_start= 'start', x_end= 'end', y= 'demo', color= 'responsible'
+    df_daily_revenue= df.groupby('date')['price'].sum().reset_index()
+
+    # index needs to be reseted => plotly express cannot handle shopping mal as index
+    df_mall_revenue= df.groupby('shopping_mall')['price'].sum().reset_index()
+
+    print(df_mall_revenue.head())
+
+    fig_line = px.line(
+        df_daily_revenue, x= 'date', y= 'price', title= 'Daily Revenue'
     )
 
-    fig.update_yaxes(autorange= 'reversed')
-    gantt_plot= plot(fig, output_type= 'div')
-    context = {'plot_div': gantt_plot}
+    fig_pie= px.pie(df_mall_revenue, values='price', names='shopping_mall', title= 'Revenue per Mall')
+
+    line_plot= plot(fig_line, output_type= 'div')
+    pie_plot= plot(fig_pie, output_type= 'div')
+    context = {
+        'line_div': line_plot,
+        'pie_div': pie_plot,
+    }
 
     return render(request, 'index.html', context)
